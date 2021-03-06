bf5180d7d5c92ae172c8bc2cbf41f5e333d9a19e
kbrzdem <kbr_ozdm@hotmail.com>
2021-03-06 21:09:26 +0300

    App.js oluşturuldu

---
+++ bitirme-projesi/src/App.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import axios from  'axios';
+
+import "./App.css";
+
+import { Header, Overview, Contagion,Symptomps, Prevention,Qestion, Footer} from "./components"
+
+function App() {
+
+  const [links, setLinks] = useState([])
+  const [indexP, setİndexP] = useState([])
+  const [contagion, setContagion] = useState([])
+  const [card, setCard] = useState([])
+  const [symptompsP, setSymptompsP] = useState([])
+  const [preventionP, setPreventionP] = useState([])
+
+  useEffect(() => {
+    axios.get('https://raw.githubusercontent.com/omergulcicek/bootcamp/master/React/data.json')
+    .then(function({data}) {
+      setLinks(data[0].headerLinks);
+      setİndexP(data[0].indexPage);
+      setContagion(data[0].contagionPage);
+      setCard(data[0].contagionPage.cards);
+      setSymptompsP(data[0].symptompsPage);
+      setPreventionP(data[0].preventionPage);
+      //*console.log(data[0].symptompsPage)
+      //*console.log(data[0].contagionPage.cards)
+      //{data} süslü parantez gelen değerin içerisindeki data demek 
+
+      })
+      .catch(function(error) {
+        console.log("hata")
+      })
+  }, [])
+  
+//useEffect(function, [])-- boş dizi(2.parametre) comp. oluşturduktan sonra bir kez çalıştırıyor
+
+
+  return (
+  <>
+    <Router>
+      <Header hlinks= {links} />
+      <main>
+        
+          <Switch>
+                <Route path="/Contagion" >
+                  <Contagion contagionP={contagion} cards={card} /> 
+                </Route>
+                <Route path="/Symptoms" >
+                  <Symptomps symptompsP= {symptompsP}  />
+                </Route>
+                <Route path="/Prevention">
+                  <Prevention preventionP= {preventionP} />
+                </Route>
+                <Route path="/" >
+                  <Overview  indexP= {indexP} />
+                </Route>
+          </Switch>
+          
+      </main>
+      <Qestion />
+      <Footer hlinks= {links} /> 
+    </Router>
+  </>  
+  );
+}
+
+export default App;
+
