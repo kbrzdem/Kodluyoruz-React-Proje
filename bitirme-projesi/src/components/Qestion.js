133c8a710680a12b4b6827e4044130a82fb1c774
kbrzdem <kbr_ozdm@hotmail.com>
2021-03-07 00:37:37 +0300

    return oluşturma

--- bitirme-projesi/src/components/Qestion.js
+++ bitirme-projesi/src/components/Qestion.js
@@ -1,7 +1,18 @@
 
 const Qestion = () => {
 
-
+    return (
+        <>
+            <section className="section6">
+                <div className="container">
+                    <h1 className="section6__title" >Have Question in mind?</h1>
+                    <h2 className="section6__title2" > Let us help you</h2>
+                    <input type="mail" placeholder="snmonydemo@gmail.com" className="section6__w100" /> 
+                        <button href="" className="button button--position">Send</button>
+                </div>
+            </section>                     
+        </>
+    )
     
 };
 
